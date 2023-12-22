01b827e7e8ff8d6e5504dd88d462f8bbf61a80e0
jAlbright2002 <G00379271@atu.ie>
2023-12-22 22:24:05 +0000

    missing import

--- src/main/java/ie/atu/userinterface/HardwareClientNew.java
+++ src/main/java/ie/atu/userinterface/HardwareClientNew.java
@@ -1,6 +1,7 @@
 package ie.atu.userinterface;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
